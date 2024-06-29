f7641f31d6f2fe04b03f2cac102f245f74757774
tair800 <tahiraa@code.edu.az>
2024-06-29 15:33:02 +0400

    fifth

--- Fiorella/Program.cs
+++ Fiorella/Program.cs
@@ -3,13 +3,14 @@ using Fiorella;
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 builder.Services.Register(config);
-
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseSession();
 app.UseRouting();
 app.UseAuthorization();
+
+
 app.MapControllerRoute(
            name: "areas",
            pattern: "{area:exists}/{controller=DashBoard}/{action=Index}/{id?}"
